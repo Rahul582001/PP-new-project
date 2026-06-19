bf3b346ba71116bd280f97c88c09d0fb94f9db48
rahulrao <rahulrao@medianv.com>
2026-06-19 11:20:15 +0530

    final

--- src/modules/webhook/webhook.service.ts
+++ src/modules/webhook/webhook.service.ts
@@ -5,6 +5,7 @@ export class WebhookService {
   webhookMock(dto: any) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { details } = dto;
+    console.log('details: ', details);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     return { details };
   }
